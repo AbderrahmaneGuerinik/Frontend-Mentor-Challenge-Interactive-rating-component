8ba93423dddc8935abe3a1a4285df1d219b677a9
AbderrahmaneGuerinik <125129120+AbderrahmaneGuerinik@users.noreply.github.com>
2023-11-14 21:16:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const click = () => {
    }
    
 const fetch = (button) => {
+   submit.removeAttribute("disabled");
    number = button.innerText;
    selected.innerText = number;
 }
@@ -23,4 +24,5 @@ buttons.forEach((button) => {
 })
 submit.addEventListener("click",click);
  
+
 
